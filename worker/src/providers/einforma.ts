da25bd18b33cc6981e0cce7c9e438ae35ea353b6
Rúben Silva <128933310+rubsil@users.noreply.github.com>
2026-08-26 17:31:01 +0000

    Fix HTML entity decoder syntax

--- worker/src/providers/einforma.ts
+++ worker/src/providers/einforma.ts
@@ -9,7 +9,7 @@ export interface EInformaResult {
 }
 
 function decodeHtml(value: string): string {
-  return value.replace(/&nbsp;/gi, " ").replace(/&amp;/gi, "&").replace(/&quot;/gi, '"').replace(/&#39;|&apos;/gi, "'").replace(/&lt;/gi, "<").replace(/&gt;/gi, ">\").replace(/&#(\d+);/g, (_, n) => String.fromCharCode(Number(n))).replace(/&#x([0-9a-f]+);/gi, (_, n) => String.fromCharCode(parseInt(n, 16)));
+  return value.replace(/&nbsp;/gi, " ").replace(/&amp;/gi, "&").replace(/&quot;/gi, '"').replace(/&#39;|&apos;/gi, "'").replace(/&lt;/gi, "<").replace(/&gt;/gi, ">").replace(/&#(\d+);/g, (_, n) => String.fromCharCode(Number(n))).replace(/&#x([0-9a-f]+);/gi, (_, n) => String.fromCharCode(parseInt(n, 16)));
 }
 
 function clean(value: string): string {
